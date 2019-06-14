495bf020682d60b1ac73acccc429b9f216ca3b70
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-06-14 12:14:41 -0400

    Complete ex2 of hastables. Tests pass.

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -10,7 +10,25 @@ char **reconstruct_trip(Ticket **tickets, int length)
   char **route = malloc(length * sizeof(char *));
 
   /* YOUR CODE HERE */
+  // Overall runtime O(n)+O(n) -> O(2n) -> O(n) -> linear
+  
+  // O(n) -> Loop through ticket list and add each ticket to hashed Linked List
+  for (int i=0; i<length; i++){
+    // Avg. O(1) -> insert into hash table: key=source value=destination
+    hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
+  }
 
+  // Start at original source = "NONE"
+  char *key = "NONE";
+  // O(n) -> Loop through hash table to create route
+  for (int i=0; i<length; i++){
+    // Avg. O(1) -> search hash table: key=source
+    key = hash_table_retrieve(ht, key); // the next key is ths source's desetination value
+    // O(1)
+    route[i] = key; // route is the source's destination or the next key
+  }
+
+  destroy_hash_table(ht);
   return route;
 }
 
